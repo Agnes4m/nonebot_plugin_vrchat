cd548bdc55c36b25c08c281f03336492bf75ff66
afu <735803792@qq.com>
2023-10-10 00:19:07 +0800

    🚀v0.0.2

--- nonebot_plugin_vrchat/__init__.py
+++ nonebot_plugin_vrchat/__init__.py
@@ -5,10 +5,10 @@ require("nonebot_plugin_saa")  # type: ignore
 from . import __main__ as __main__  # noqa: E402
 from .config import ConfigModel  # noqa: E402
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __plugin_meta__ = PluginMetadata(
-    name="Sekai Stickers",
-    description="使用第三方api实现vrchat相关操作",
+    name="VRChat查询",
+    description="使用第三方api实现vrchat相关操作，例如查询好友状态",
     usage="使用【vrc帮助】指令获取帮助",
     type="application",
     homepage="https://github.com/Agnes4m/nonebot_plugin_vrchat",
