e8e1dc26d1c6d98f433980c24de7dbe44bc4401a
student_2333 <lgc2333@126.com>
2023-12-19 09:12:23 +0800

    bump

--- nonebot_plugin_vrchat/__init__.py
+++ nonebot_plugin_vrchat/__init__.py
@@ -7,7 +7,7 @@ require("nonebot_plugin_saa")
 from . import __main__ as __main__  # noqa: E402
 from .config import EnvConfig  # noqa: E402
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __plugin_meta__ = PluginMetadata(
     name="VRChat查询",
     description="使用第三方 API SDK 实现 VRChat 相关操作，例如查询好友状态",
