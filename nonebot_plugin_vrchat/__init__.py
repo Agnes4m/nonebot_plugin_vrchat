7a35620a994681d80e77cac20d13b408adaf3954
Agnes Digital <Z735803792@163.com>
2025-06-09 13:55:05 +0800

    🐛修复跨适配器插件错误

--- nonebot_plugin_vrchat/__init__.py
+++ nonebot_plugin_vrchat/__init__.py
@@ -2,7 +2,7 @@ from nonebot import require
 from nonebot.plugin import PluginMetadata, inherit_supported_adapters
 
 require("nonebot_plugin_session")
-require("nonebot_plugin_saa")
+require("nonebot_plugin_alconna")
 
 from . import __main__ as __main__  # noqa: E402
 from .config import EnvConfig  # noqa: E402
@@ -15,7 +15,7 @@ __plugin_meta__ = PluginMetadata(
     type="application",
     homepage="https://github.com/Agnes4m/nonebot_plugin_vrchat",
     config=EnvConfig,
-    supported_adapters=inherit_supported_adapters("nonebot_plugin_saa"),
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
     extra={
         "version": __version__,
         "author": ["Agnes4m <Z735803792@163.com>", "student_2333 <lgc2333@126.com>"],
