ebe2fe80ff9dd20cbd478674cb935e7c339026ba
AgnesDigital <Z735803792@163.com>
2026-02-26 15:12:20 +0800

    🐛修复依赖问题

--- nonebot_plugin_vrchat/utils.py
+++ nonebot_plugin_vrchat/utils.py
@@ -1,19 +1,23 @@
 from pathlib import Path
 from typing import Any
 
-from ruamel.yaml import YAML
-
-yaml = YAML(typ="safe")
-yaml.indent = 2
-yaml.allow_unicode = True
-yaml.default_flow_style = False
+import yaml
 
 
 def load_yaml(file_path: Path) -> Any:
-    with file_path.open("r", encoding="u8") as f:
-        return yaml.load(f)
+    """加载 YAML 文件"""
+    with file_path.open("r", encoding="utf-8") as f:
+        return yaml.safe_load(f)
 
 
 def dump_yaml(file_path: Path, data: Any) -> None:
-    with file_path.open("w", encoding="u8") as f:
-        yaml.dump(data, f)
+    """保存 YAML 文件"""
+    with file_path.open("w", encoding="utf-8") as f:
+        yaml.safe_dump(
+            data,
+            f,
+            allow_unicode=True,
+            default_flow_style=False,
+            indent=2,
+            sort_keys=False,
+        )
